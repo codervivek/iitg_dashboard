78decf6eebc2d18779a89534e0861defade4871c
yagyanshbhatia <yagyansh1999@gmail.com>
2018-09-29 10:03:28 +0530

    Add calender post API code

--- Integrations/calender API/quickstart.py
+++ Integrations/calender API/quickstart.py
@@ -5,7 +5,7 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -31,6 +31,31 @@ def main():
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
+
+    event = {
+      'summary': 'Yagyansh Testing the calender post API',
+      'location': 'C-331 lohit hostel IITG',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': '2018-09-27T09:00:00',
+        'timeZone': 'Asia/Kolkata',
+      },
+      'end': {
+        'dateTime': '2018-09-27T17:00:00',
+        'timeZone': 'Asia/Kolkata',
+      },
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
+
 
 if __name__ == '__main__':
     main()
